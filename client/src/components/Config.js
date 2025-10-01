c50b8cba4d27634cdaa08a3f305ea7700c5a6810
현욱 주 <lite1324@naver.com>
2025-10-01 18:14:09 +0900

    themoviedb API 설정

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,11 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+//IMAGE SIZE
+// export const BACKDROP_SIZE = 'w1280';
+// export const POSTER_SIZE = 'w500';
+// export const PROFILE_SIZE = 'w185';
